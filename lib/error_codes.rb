57b283d160d9f4c51bf3347a5d16f9981cfbeea3
j.taneichi <j.tane3ma@gmail.com>
2015-11-26 08:32:52 +0900

    bundle install bcrypt

---
+++ lib/error_codes.rb
@@ -0,0 +1,22 @@
+module ErrorCodes
+  INVALID_TOKEN           = 1
+  BROKEN_TOKEN            = 2
+  NOT_SELF                = 3
+  INVALID_PIN             = 4
+  FAIL_SAVE               = 5
+  FAIL_DESTROY            = 6
+  FAIL_FIND_BY_ID         = 7
+  NOT_FOUND_USER          = 8
+  SERIAL_PARAMS           = 9
+  DATA_PARAM_NIL          = 10
+  NO_DEVICES              = 11
+  DEVICE_ID_NOT_SPECIFIED = 12
+  DEVICE_NOT_FOUND        = 13
+  TIME_SPAN_NOT_SPECIFIED = 14
+  INVALID_TIME_SPAN       = 15
+  INTERNAL_SERVER_ERROR   = 16
+  TOO_MANY_REQUESTS       = 17
+  NOT_FOUND               = 18
+  TIMEOUT                 = 19
+  ALREADY_EXISTING         = 20
+end
