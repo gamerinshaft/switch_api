b62cd73359b29c78573d3e4c3df09fa40978184c
j.taneichi <j.tane3ma@gmail.com>
2015-11-26 08:35:22 +0900

    create sign up api

--- app/apis/api/v1/authorize.rb
+++ app/apis/api/v1/authorize.rb
@@ -1,11 +1,13 @@
 module API
   module V1
     class Authorize < Grape::API
+
       helpers do
         params :attributes do
-          requires :name, type: String, desc: "User name."
-          requires :email, type: String, desc: "User email."
-          requires :password, type: String, desc: "User Password."
+          requires :screen_name, type: String, desc: "表示名"
+          requires :email, type: String, desc: "メールアドレス"
+          requires :password, type: String, desc: "パスワード"
+          requires :auth_token, type: String, desc: "トークン"
           # optional :body, type: String, desc: "MessageBoard body."
         end
       end
@@ -22,7 +24,23 @@ module API
           use :attributes
         end
         post '/signup', jbuilder: 'api/v1/auth/signup' do
-          @hoge = params[:name]
+          if (token = AuthToken.find_by(token: params[:auth_token]))
+            if user.info != nil
+              error!(json: {
+                 errors: [
+                     message: ('errors.messages.already_existing'),
+                     code: ErrorCodes::ALREADY_EXISTING
+                 ]
+              }, status: 400)
+            else
+              user_info = user.build_info(
+                  email:           params[:email],
+                  screen_name:     params[:screen_name],
+                  password:        params[:password]
+              )
+              @hoge = save_object(user_info)
+            end
+          end
         end
       end
     end
