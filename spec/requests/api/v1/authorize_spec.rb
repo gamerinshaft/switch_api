c976fe5dce5af390f317e99f1379447ea2ff33f7
j.taneichi <j.tane3ma@gmail.com>
2015-11-27 20:30:24 +0900

    test 書いた

--- spec/requests/api/v1/authorize_spec.rb
+++ spec/requests/api/v1/authorize_spec.rb
@@ -4,9 +4,16 @@ describe "Authorize" do
   describe "GET /api/v1/auth/token"do
     before(:all) do
       get "/api/v1/auth/token"
+      @json = JSON.parse(response.body)
     end
     it 'return 200 status' do
       expect(response.status).to be 200
+    end
+    it 'meta code is same status' do
+      expect(@json["meta"]["code"]).to be 200
+    end
+    it 'response is not nil' do
+      expect(@json["response"]).not_to be_empty
     end
   end
 end
