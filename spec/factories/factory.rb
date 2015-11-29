73678c77f66b84a4b13113d8b3e3339d1f4c0f82
j.taneichi <j.tane3ma@gmail.com>
2015-11-29 13:51:49 +0900

    add sequence

--- spec/factories/factory.rb
+++ spec/factories/factory.rb
@@ -1,16 +1,7 @@
+require 'faker'
 FactoryGirl.define do
-  factory :user do
-
-  end
-  factory :auth_token do
-    user
-    token "hoehoge"
-  end
-  factory :user_info do
-    user
-    name "random"
-    email "random@gmail.com"
-    screen_name "random"
-    password "random"
-  end
+  sequence(:screen_name){ Faker::Name.first_name }
+  sequence(:email){ Faker::Internet.email }
+  sequence(:password){ Faker::Internet.password }
+  sequence(:auth_token){ SecureRandom.urlsafe_base64 }
 end
