013a09ea2a4bad5a2553be9ea10d11bb8d608727
j.taneichi <j.tane3ma@gmail.com>
2015-12-03 10:59:51 +0900

    add migration references

--- db/migrate/20151202234724_create_task_infrareds.rb
+++ db/migrate/20151202234724_create_task_infrareds.rb
@@ -1,6 +1,8 @@
 class CreateTaskInfrareds < ActiveRecord::Migration
   def change
     create_table :task_infrareds do |t|
+      t.references :task, index: true, foreign_key: true
+      t.references :infrared, index: true, foreign_key: true
 
       t.timestamps null: false
     end
