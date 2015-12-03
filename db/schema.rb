aee1430361e730302c7d4a2cb5a74e30dfc9963e
j.taneichi <j.tane3ma@gmail.com>
2015-12-03 13:02:50 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151203035051) do
+ActiveRecord::Schema.define(version: 20151203040210) do
 
   create_table "auth_tokens", force: :cascade do |t|
     t.string   "token"
@@ -28,8 +28,10 @@ ActiveRecord::Schema.define(version: 20151203035051) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "log_id"
   end
 
+  add_index "infrareds", ["log_id"], name: "index_infrareds_on_log_id"
   add_index "infrareds", ["user_id"], name: "index_infrareds_on_user_id"
 
   create_table "logs", force: :cascade do |t|
@@ -55,8 +57,10 @@ ActiveRecord::Schema.define(version: 20151203035051) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "log_id"
   end
 
+  add_index "tasks", ["log_id"], name: "index_tasks_on_log_id"
   add_index "tasks", ["user_id"], name: "index_tasks_on_user_id"
 
   create_table "user_infos", force: :cascade do |t|
