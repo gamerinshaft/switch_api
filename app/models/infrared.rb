9caa779b323135693f1b1bb5c83257fb6bcd09a4
j.taneichi <j.tane3ma@gmail.com>
2015-12-04 22:43:16 +0900

    fix

--- app/models/infrared.rb
+++ app/models/infrared.rb
@@ -1,6 +1,6 @@
 class Infrared < ActiveRecord::Base
   belongs_to :user
   has_many :infrared_relationals
-  has_many :tasks, through: :infrared_relationals
+  has_many :infrared_groups, through: :infrared_relationals
   belongs_to :log
 end
