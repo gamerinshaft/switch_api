4387bcb4d6b73f38cedd06ef22041c9e9ac53ce1
j.taneichi <j.tane3ma@gmail.com>
2015-12-04 22:28:16 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151204131528) do
+ActiveRecord::Schema.define(version: 20151204132603) do
 
   create_table "auth_tokens", force: :cascade do |t|
     t.string   "token"
@@ -21,6 +21,17 @@ ActiveRecord::Schema.define(version: 20151204131528) do
   end
 
   add_index "auth_tokens", ["user_id"], name: "index_auth_tokens_on_user_id"
+
+  create_table "infrared_groups", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.integer  "log_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "infrared_groups", ["log_id"], name: "index_infrared_groups_on_log_id"
+  add_index "infrared_groups", ["user_id"], name: "index_infrared_groups_on_user_id"
 
   create_table "infrareds", force: :cascade do |t|
     t.string   "name"
