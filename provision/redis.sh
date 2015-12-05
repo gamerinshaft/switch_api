aebfe421aca6bcd3f2b435f44835362d69f94ccf
j.taneichi <j.tane3ma@gmail.com>
2015-12-06 00:26:00 +0900

    Revert "Merge branch 'develop' of https://github.com/gamerinshaft/switch_api into feature/issues/#53"
    This reverts commit 307018494f4a774f61093356c9328bfc8cde6888, reversing
    changes made to cb4d7aa5cba0990000a5f72ff06efee688b71651.

--- provision/redis.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-sudo rpm --import http://rpms.famillecollet.com/RPM-GPG-KEY-remi
-sudo rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
-
-sudo yum -y install redis --enablerepo=remi
-sudo chkconfig redis on
-sudo service redis start
