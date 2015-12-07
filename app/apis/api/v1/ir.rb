68b514d75ca46992cbf078fe1acea15c986357ff
j.taneichi <j.tane3ma@gmail.com>
2015-12-07 21:00:35 +0900

    update ir.rb

--- app/apis/api/v1/ir.rb
+++ app/apis/api/v1/ir.rb
@@ -4,19 +4,43 @@ module API
   module V1
     class IR < Grape::API
       resource :ir do
-        desc 'LEDの点滅', notes: <<-NOTE
-            <h1>LEDを点滅させる</h1>
+        desc '赤外線の受信', notes: <<-NOTE
+            <h1>赤外線を受信します</h1>
             <p>
-               LEDがつくよ
+              赤外線を受信します
             </p>
           NOTE
         params do
           requires :auth_token, type: String, desc: 'Auth token.'
         end
-        get '/recieve', jbuilder: 'api/v1/ir/recieve' do
-          path = Rails.root.to_s
-          command = File.join(path, "commands/recieve")
-          `#{command} #{path}/data/`
+        post '/recieve', jbuilder: 'api/v1/ir/recieve' do
+          if (token = AuthToken.find_by(token: params[:auth_token]))
+            if user.info.nil?
+              error!(meta: {
+                       status: 400,
+                       errors: [
+                         message: ('errors.messages.user_not_found'),
+                         code: ErrorCodes::NOT_FOUND_USER
+                       ]
+                     }, response: {})
+            else
+              infrared = user.infrareds.create(name: "名無しの赤外線", data: "")
+              path = Rails.root.to_s
+              command = File.join(path, "commands/recieve")
+              fname = "user_#{user.id}_ir_#{infrared.id}.txt"
+              `#{command} #{path}/data/#{fname}`
+              infrared.update(data: "#{fname}")
+              @infrared = infrared
+            end
+          else
+            error!(meta: {
+               status: 400,
+               errors: [
+                 message: ('errors.messages.invalid_token'),
+                 code: ErrorCodes::INVALID_TOKEN
+               ]
+             }, response: {})
+          end
         end
       end
     end
