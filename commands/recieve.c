d621b5845a79dd8b031b12aaba8e35b1b9dbaae3
j.taneichi <j.tane3ma@gmail.com>
2015-12-07 21:00:50 +0900

    create recieve.c

--- commands/recieve.c
+++ commands/recieve.c
@@ -3,17 +3,17 @@
 
 int main(int argc, char *argv[]){
   char *path;
+  char *fileName;
   if(argc >= 2){
-   path = argv[1];
+   fileName = argv[1];
   }else{
     printf("Not select path!\n");
     return 0;
   }
 
   FILE *fp;
-  char *fileName = "irdata.txt";
-  strcat(path, fileName);
-  fp = fopen(path, "w");
+
+  fp = fopen(fileName, "w");
   fprintf(fp, "Test\n");
   fclose(fp);
   return 0;
