a5862ad08a3f62319dd47e265da08902407e95c5
j.taneichi <j.tane3ma@gmail.com>
2015-12-09 01:21:13 +0900

    remove column

--- db/migrate/20151204132603_create_infrared_groups.rb
+++ db/migrate/20151204132603_create_infrared_groups.rb
@@ -3,7 +3,6 @@ class CreateInfraredGroups < ActiveRecord::Migration
     create_table :infrared_groups do |t|
       t.string :name
       t.references :user, index: true, foreign_key: true
-      t.references :log, index: true, foreign_key: true
 
       t.timestamps null: false
     end
