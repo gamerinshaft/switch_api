d65ef5759111cab667e7a04408e4fbc2a3523643
j.taneichi <j.tane3ma@gmail.com>
2015-12-09 15:20:48 +0900

    remove test file

--- app/apis/api/v1/infrared_group.rb
+++ app/apis/api/v1/infrared_group.rb
@@ -4,15 +4,6 @@ module API
   module V1
     class InfraredGroup < Grape::API
       resource :group do
-        desc 'グループ内の赤外線一覧の表示', notes: <<-NOTE
-            <h1>グループ内の赤外線を表示</h1>
-            <p>
-              グループ内の赤外線を表示します
-            </p>
-          NOTE
-        get '/ping', jbuilder: 'api/v1/group/ping' do
-        end
-
         desc 'グループの一覧表示', notes: <<-NOTE
             <h1>グループを一覧表示する</h1>
             <p>
