880c1595815d3cda097f1fda618607555df7d786
j.taneichi <j.tane3ma@gmail.com>
2015-12-11 23:34:45 +0900

    rails g model Schedule name:string description:text cron:string job_name:string user:references

---
+++ db/migrate/20151211143436_create_schedules.rb
@@ -0,0 +1,13 @@
+class CreateSchedules < ActiveRecord::Migration
+  def change
+    create_table :schedules do |t|
+      t.string :name
+      t.text :description
+      t.string :cron
+      t.string :job_name
+      t.references :user, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
