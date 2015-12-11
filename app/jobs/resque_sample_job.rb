7339f75a1f40a57bad59bebf113e3df5822362a6
j.taneichi <j.tane3ma@gmail.com>
2015-12-11 23:26:41 +0900

    rename

--- app/jobs/resque_sample_job.rb
+++ app/jobs/resque_sample_job.rb
@@ -1,7 +1,7 @@
-class ResqueSampleJob
-  @queue = :resque_sample_job
+class ResqueScheduleTask
+  @queue = :resque_schdule_task
   def self.perform(text)
-    path = File.expand_path("log/resque_sample.log", Rails.root)
+    path = File.expand_path("log/resque_schedule_task.log", Rails.root)
     File.open(path, 'a') do |f|
       f.puts text["screen_name"]
     end
