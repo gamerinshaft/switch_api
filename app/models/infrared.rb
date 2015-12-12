0fa99ba110b396681eb219f5a950074eeed2b81f
j.taneichi <j.tane3ma@gmail.com>
2015-12-13 06:39:52 +0900

    add relation

--- app/models/infrared.rb
+++ app/models/infrared.rb
@@ -1,7 +1,7 @@
 class Infrared < ActiveRecord::Base
   belongs_to :user
-  has_many :logs
   has_many :infrared_relationals
   has_many :infrared_groups, through: :infrared_relationals
+  has_many :logs, as: :loggable
   has_one :schedule
 end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,5 +1,6 @@
 class Schedule < ActiveRecord::Base
   belongs_to :user
   belongs_to :infrared
+  has_many :logs, as: :loggable
   enum status: %i(inactive_schedule active_schedule)
 end
