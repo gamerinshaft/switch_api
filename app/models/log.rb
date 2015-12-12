028fc43002ee76c0666e441fc46b02fde7025e34
j.taneichi <j.tane3ma@gmail.com>
2015-12-13 04:15:54 +0900

    add new status

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,5 +1,5 @@
 class Log < ActiveRecord::Base
   belongs_to :user
   belongs_to :infrared, dependent: :destroy
-  enum status: %i(recieve_ir send_ir update_ir destroy_ir add_ir remove_ir robot_send_ir remove_schedule create_schedule activate_schedule update_schedule)
+  enum status: %i(recieve_ir send_ir update_ir destroy_ir add_ir remove_ir robot_send_ir remove_schedule create_schedule activate_schedule update_schedule delete_schedule)
 end
