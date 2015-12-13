a74c008acccb82d43551b2821b4d050d4a74d305
j.taneichi <j.tane3ma@gmail.com>
2015-12-13 21:30:50 +0900

    add log methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,14 @@ class User < ActiveRecord::Base
   has_many :infrareds
   has_many :infrared_groups
   has_many :logs, dependent: :destroy
-  has_many :infrared_logs, through: :logs, source: :loggable, source_type: 'Infrared'
-  has_many :schedule_logs, through: :logs, source: :loggable, source_type: 'Schedule'
+  has_many :log_infrareds, through: :logs, source: :loggable, source_type: 'Infrared'
+  has_many :log_schedules, through: :logs, source: :loggable, source_type: 'Schedule'
+
+  def self.schedule_logs
+    self.logs.where(loggable_type: "Schedule")
+  end
+
+  def self.infrared_logs
+    self.logs.where(loggable_type: "Infrared")
+  end
 end
