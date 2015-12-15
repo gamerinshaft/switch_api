14250a9a9511a315513fd868b3f54a3d7ffb688b
j.taneichi <j.tane3ma@gmail.com>
2015-12-15 20:29:34 +0900

    add relational

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
 
   has_many :auth_tokens, dependent: :destroy
   has_one :info, class_name: 'UserInfo', dependent: :destroy
+  has_one :room, dependent: :destroy
   has_many :schedules, dependent: :destroy
   has_many :infrareds
   has_many :infrared_groups
