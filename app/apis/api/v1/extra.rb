96a54183828b10cd2f8fbae96111f151783e0561
j.taneichi <j.tane3ma@gmail.com>
2015-12-15 21:26:13 +0900

    update extra

--- app/apis/api/v1/extra.rb
+++ app/apis/api/v1/extra.rb
@@ -2,19 +2,55 @@ module API
   module V1
     class Extra < Grape::API
       resource :extra do
-        desc 'ping', notes: <<-NOTE
-            <h1>レスポンスの有無</h1>
+        desc '気温を取得する', notes: <<-NOTE
+            <h1>ログを取得します</h1>
+            <h3>size - 取得数</h3>
             <p>
-            このURLにリクエストすることによって、アクセストークンを取得すること\
-ができます。<br>
-            アクセストークンは基本的にどんなリクエストをする時でも必要なので、\\
-値をキャッシュするようにしてください。
+              null ・・・ すべての気温の取得<br>
+              num  ・・・ 最新num件の気温を取得
             </p>
           NOTE
-        get '/ping', jbuilder: 'api/v1/extra/ping' do
-          path = Rails.root.to_s
-          command = File.join(path, 'commands/temperature.rb')
-          `sudo ruby #{command} #{path}/data/extra/temperature.txt`
+        params do
+          requires :auth_token, type: String, desc: 'Auth token.'
+          optional :size, type: Integer, desc: 'Array size'
+        end
+        get '/temperatures', jbuilder: 'api/v1/extra/temperatures' do
+          if (token = AuthToken.find_by(token: params[:auth_token]))
+            if user.info.nil?
+              error!(meta: {
+                       status: 400,
+                       errors: [
+                         message: ('errors.messages.user_not_found'),
+                         code: ErrorCodes::NOT_FOUND_USER
+                       ]
+                     }, response: {})
+            else
+              @user = user
+              if user.room.nil?
+                if params[:size].nil?
+                  @logs = user.room.temperatures.sort { |a, b| b <=> a }
+                else
+                  @logs = user.room.temperatures.last(params[:size].to_i).sort { |a, b| b <=> a }
+                end
+              else
+                error!(meta: {
+                         status: 400,
+                         errors: [
+                           message: ('errors.messages.room_not_found'),
+                           code: ErrorCodes::NOT_FOUND_ROOM
+                         ]
+                       }, response: {})
+              end
+            end
+          else
+            error!(meta: {
+                     status: 400,
+                     errors: [
+                       message: ('errors.messages.invalid_token'),
+                       code: ErrorCodes::INVALID_TOKEN
+                     ]
+                   }, response: {})
+          end
         end
       end
     end
