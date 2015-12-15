7c9fd05885e71d42220b5c899124efe1e796aab9
j.taneichi <j.tane3ma@gmail.com>
2015-12-15 21:55:36 +0900

    cerate temperatures api

--- app/apis/api/v1/extra.rb
+++ app/apis/api/v1/extra.rb
@@ -2,6 +2,38 @@ module API
   module V1
     class Extra < Grape::API
       resource :extra do
+        desc '気温を取得を始める', notes: <<-NOTE
+            <h1>cronをまわします</h1>
+          NOTE
+        params do
+          requires :auth_token, type: String, desc: 'Auth token.'
+        end
+        post '/temperature/start', jbuilder: 'api/v1/extra/temperature_start'  do
+          if (token = AuthToken.find_by(token: params[:auth_token]))
+            if user.info.nil?
+              error!(meta: {
+                       status: 400,
+                       errors: [
+                         message: ('errors.messages.user_not_found'),
+                         code: ErrorCodes::NOT_FOUND_USER
+                       ]
+                     }, response: {})
+            else
+              @user = user
+              job = "user_#{user.id}_temperature_get_cycle"
+              Resque.set_schedule(job, class: 'ResqueTemperatureGetJob', cron: "* * * * *", args: user)
+            end
+          else
+            error!(meta: {
+                     status: 400,
+                     errors: [
+                       message: ('errors.messages.invalid_token'),
+                       code: ErrorCodes::INVALID_TOKEN
+                     ]
+                   }, response: {})
+          end
+        end
+
         desc '気温を取得する', notes: <<-NOTE
             <h1>ログを取得します</h1>
             <h3>size - 取得数</h3>
@@ -41,6 +73,36 @@ module API
                          ]
                        }, response: {})
               end
+            end
+          else
+            error!(meta: {
+                     status: 400,
+                     errors: [
+                       message: ('errors.messages.invalid_token'),
+                       code: ErrorCodes::INVALID_TOKEN
+                     ]
+                   }, response: {})
+          end
+        end
+        desc '気温取得ルーティンの停止', notes: <<-NOTE
+            <h1>気温取得ルーティンを停止します</h1>
+          NOTE
+        params do
+          requires :auth_token, type: String, desc: 'Auth token.'
+        end
+        post '/temperature/remove', jbuilder: 'api/v1/extra/temperature_remove' do
+          if (token = AuthToken.find_by(token: params[:auth_token]))
+            if user.info.nil?
+              error!(meta: {
+                       status: 400,
+                       errors: [
+                         message: ('errors.messages.user_not_found'),
+                         code: ErrorCodes::NOT_FOUND_USER
+                       ]
+                     }, response: {})
+            else
+              job = "user_#{user.id}_temperature_get_cycle"
+              Resque.remove_schedule(job)
             end
           else
             error!(meta: {
